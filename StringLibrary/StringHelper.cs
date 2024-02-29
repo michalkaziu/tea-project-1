7a52c54e06ad4930ef781281cdb6d625209135f5
Michał Chmielewski <kaziureg@gmail.com>
2024-02-29 01:00:53 +0100

    StringHelper

---
+++ StringLibrary/StringHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace StringLibrary;
+
+public static class StringHelper
+{
+    public static bool CutFromHtml(ref string html, string search, string opening, string closing)
+    {
+        var searchIndex = html.IndexOf(search);
+        if (searchIndex == -1)
+            return false;
+
+        var openingIndex = html.Substring(0, searchIndex).LastIndexOf(opening);
+        if (openingIndex == -1)
+            return false;
+
+        var closingIndex = html.IndexOf(closing, searchIndex);
+        if (closingIndex == -1)
+            return false;
+
+        var sb = new StringBuilder();
+        sb.Append(html.Substring(0, openingIndex));
+        sb.Append(html.Substring(closingIndex + closing.Length));
+
+        html = sb.ToString();
+        return true;
+    }
+}
